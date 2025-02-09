e039b57309a5a782eedde9e2b2dd4100ba87fd46
ahmed maghraby <38741761+ahmedmaghraby@users.noreply.github.com>
2025-02-09 16:52:03 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="app">
+    <Analytics mode="production" />
     <Navbar />
     <main class="min-h-screen pt-20 bg-slate-100 dark:bg-gray-800">
       <RouterView />
     </main>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/vue';
+</script>
+ 
